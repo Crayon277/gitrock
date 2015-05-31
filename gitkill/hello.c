bd4fd950e33bc607df612155d7b1683a156af5ac
Crayon <chenye626@gmail.com>
2015-05-31 14:33:01 +0800

    see if gitkill added

---
+++ gitkill/hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: Crayon Chaney
+	> Mail:mmmmmcclxxvii@gmail.com
+	> Created Time: Sun May 31 12:05:45 2015
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	printf("hello world\n");
+	return 0;
+}
